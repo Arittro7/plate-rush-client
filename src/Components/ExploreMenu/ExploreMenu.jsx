af959800c0285f5ff397487fec5ec1a8cba7ef5d
Arittro7 <arittrowpdev@gmail.com>
2025-01-11 12:33:42 +0600

    Menu section added

---
+++ src/Components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {menu_list} from '../../assets/assets'; 
+
+const ExploreMenu = ({category, setCategory}) => {
+
+  return (
+    <div className='mt-10 text-center'>
+      <h1 className='text-3xl font-semibold'>Explore Menu</h1>
+      <p>Choose your desire food from our menu</p>
+      <div
+      className='flex gap-8 justify-center mt-5 overflow-x-auto overflow-hidden'>
+        {menu_list.map((item, index) => {
+          return (
+            <div
+            onClick={() => setCategory(prev=>prev===item.menu_name ? 'all' : item.menu_name)} 
+            className='text-center' key={index}>
+              <img className={`w-[7.5vh] min-w-24 cursor-pointer ${
+      category === item.menu_name ? 'border-4 border-green-500 p-1 rounded-full' : ''
+    }`} src={item.menu_image} alt="" />
+              <p className='cursor-pointer text-xl mt-3'>{item.menu_name}</p>
+            </div>
+          )
+        })}
+      </div>
+      <hr className='border-2 my-8 border-gray-300' />
+    </div>
+  );
+};
+
+export default ExploreMenu;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+import ExploreMenu from "../../Components/ExploreMenu/ExploreMenu";
 import Banner from "./Banner/Banner";
 
 const Home = () => {
+  const [category, setCategory] = useState('all');
   return (
     <div>
       <Banner></Banner>
+      <ExploreMenu category={category} setCategory={setCategory}></ExploreMenu>
     </div>
   );
 };
