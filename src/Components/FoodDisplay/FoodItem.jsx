18de88270be99b104348bbdca5c2b056c38166e7
Arittro7 <arittrowpdev@gmail.com>
2025-01-12 08:52:03 +0600

    food card design done

--- src/Components/FoodDisplay/FoodItem.jsx
+++ src/Components/FoodDisplay/FoodItem.jsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import { assets } from '../../assets/assets';
+import React, { useState } from "react";
+import { assets } from "../../assets/assets";
 
-const FoodItem = ({id, name, price, image, description}) => {
+const FoodItem = ({ id, name, price, image, description }) => {
+  const [itemCount, setItemCount] = useState(0);
   return (
-    <div className='mb-3'>
+    <div className="mb-3">
       <div className="card card-compact bg-base-100 w-96 shadow-xl">
-  <figure>
-    <img className='p-4 rounded-xl'
-      src={image}
-      alt="food item" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">{name}</h2>
-    <img className='w-24' src={assets.rating_starts} alt="" />
-    <p>{description}</p>
-    <p className='text-xl font-semibold'>${price}</p>
-    <div className="card-actions justify-center">
-      <button className="btn btn-primary">Order</button>
-    </div>
-  </div>
-</div>
+        <div className="relative">
+          <img className="p-4" src={image} alt="food item" />
+          {!itemCount ? (
+            <img  
+              className="absolute bottom-5 right-5 w-10 cursor-pointer"
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_white}
+            />
+          ) : (
+            <div className="absolute bottom-5 right-5 flex gap-2 bg-white px-1 py-1 rounded-full">
+              <img 
+              className="cursor-pointer"
+              onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
+              <p className="text-lg font-semibold">{itemCount}</p>
+              <img
+              className="cursor-pointer" 
+              onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+            </div>
+          )}
+        </div>
+        <div className="card-body">
+          <div className="p-4 bg-gray-100 rounded-xl">
+            <h2 className="card-title">{name}</h2>
+            <img className="w-24" src={assets.rating_starts} alt="" />
+            <p>{description}</p>
+            <div className="card-actions justify-center mt-4 items-center">
+              <p className="text-2xl text-orange-500 font-semibold">${price}</p>
+              <button className="btn px-4 bg-orange-500 hover:bg-green-400">
+                Order Now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
